bffcd94f60aa09f9255a820a14c3ac3fda8bbf4f
akbardevelopment <akbardaminov51@gmail.com>
2021-09-14 19:46:22 +0500

    Salom

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-$("bot").click(function(){
-    $(".container1").show();
+$(".bot").click(function(){
+    $(".container1").fadeToggle(1000);
   });
+
+  $(".botdespir1").click(function(){
+    $(".des").slideToggle (1000);
+  });
+
+  $(".botdespir3").click(function(){
+    $(".pirog").slideToggle (1000);
+  });
+
+  $(".botdespir2").click(function(){
+    $(".torti").slideToggle (1000);
+  });
+  $(".bot1").click(function(){
+    $(".container1").fadeToggle(1000)
+    {
+      $(".torti").slideToggle(1000)
+    };
+  });
+  $(".bot2").click(function(){
+  $(".container1").fadeToggle(1000)
+  {
+  $(".pirog").slideToggle(1000)
+  }
+  });
+
+  $(".bot3").click(function(){
+    $(".container1").fadeToggle(1000)
+    {
+    $(".des").slideToggle(1000)
+    }
+    });
